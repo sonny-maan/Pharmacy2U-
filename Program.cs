5524cc03000173448bd3c14043714210fe54fc96
sonny-maan <sonny_maan@yahoo.com>
2020-03-11 22:13:37 +0000

    ran dotnet new console

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pharmacy2U_Tech_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
